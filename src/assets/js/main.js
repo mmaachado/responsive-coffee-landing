55cfe42c5c96fb8842092d7aab1f6e4cca627ff1
Marcelo Machado <mjoelmachadojr@gmail.com>
2022-05-19 03:34:09 -0300

    header && nav
    header && nav section done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,17 @@
 /*===== SHOW MENU =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
 
 /*===== MENU HIDDEN =====*/
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
 
 /*===== REMOVE MENU MOBILE =====*/
 
